b98066cc185f32f1868b90d8130da764f1b04892
roshanvarma007 <roshanvarma352@gmail.com>
2023-12-25 21:01:40 +0530

    Added More Card Design

--- app/src/main/java/com/example/learn_card_view/MainActivity.java
+++ app/src/main/java/com/example/learn_card_view/MainActivity.java
@@ -32,18 +32,39 @@ public class MainActivity extends AppCompatActivity {
         shayari_name_list.add("Love Shayari");
         shayari_name_list.add("Heart Break");
         shayari_name_list.add("Sad Shayari");
+        shayari_name_list.add("Motivation Shayari");
+        shayari_name_list.add("Hug Shayari");
+        shayari_name_list.add("Kiss Shayari");
+        shayari_name_list.add("Attitude Shayari");
+        shayari_name_list.add("Good Morning");
 
         shayari_discription.add("Love is seasoned feelings of heart & needs to be expressed");
         shayari_discription.add("Trying to move on from something that broke your heart could be the worst feeling ever.");
         shayari_discription.add("Trying to move on from something that broke your heart could be the worst feeling ever.");
+        shayari_discription.add("You are the only one who can limit your greatness.");
+        shayari_discription.add("A long tight hug from you is perfect any day and every day..");
+        shayari_discription.add(" kiss shayari to express your love and passion.");
+        shayari_discription.add("Your Attitude shows what type of person you are.");
+        shayari_discription.add("Your Attitude shows what type of person you are.");
+        shayari_discription.add("Good Mornning Hav A Nice Day");
+
+
 
         shayari_image.add(R.drawable.love_shayari);
         shayari_image.add(R.drawable.heart_break);
         shayari_image.add(R.drawable.sad_imoji);
+        shayari_image.add(R.drawable.motivation_logo);
+        shayari_image.add(R.drawable.hug_logo);
+        shayari_image.add(R.drawable.kiss_logo);
+        shayari_image.add(R.drawable.attitude_logo);
+        shayari_image.add(R.drawable.good_mrng_logo);
 
         adapter = new recyclerview_adapter(shayari_name_list,shayari_discription,shayari_image,MainActivity.this);
 
         recyclerView.setAdapter(adapter);
 
+
+
     }
+
 }
